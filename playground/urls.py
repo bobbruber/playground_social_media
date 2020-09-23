675865d136835385200ae1faa10c34d07c61536f
robert bruber <robert bruber@LAPTOP-IFQVOTJ3>
2020-09-23 17:54:01 -0500

    Add project files.

---
+++ playground/urls.py
@@ -0,0 +1,45 @@
+"""
+Definition of urls for playground.
+"""
+
+from datetime import datetime
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from app import forms, views
+from django.conf.urls import include
+from django.contrib import admin
+from django.conf.urls import url
+admin.autodiscover()
+
+
+
+urlpatterns = [
+   
+    path('', views.home, name='home'),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
+    path('login/',
+         LoginView.as_view
+         (
+             template_name ='app/login.html',
+             authentication_form =forms.BootstrapAuthenticationForm,
+             extra_context =
+             {
+                 'title': 'Log in',
+                 'year' : datetime.now().year,
+             }
+         ),
+         name='login'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
+    path('admin/', admin.site.urls),
+    url(r'^logout',
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', (admin.site.urls)),
+
+    django.contrib.auth.views.logout,
+    {
+        'template_name': 'app/loggedoff.html',
+        # 'next_page': '/',
+    },
+    name='logout'),
+]
